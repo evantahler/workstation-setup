99ab16c299a3a84815386648a4c8834486297cb8
James Hart <james.hart@voom.flights>
2019-06-18 10:13:02 -0700

    Allow android.sh to be executed by itself.

--- scripts/opt-in/android.sh
+++ scripts/opt-in/android.sh
@@ -1,6 +1,8 @@
 # Don't stop if android fails
 set +e
 
+source scripts/helpers/brew.sh
+
 brew tap AdoptOpenJDK/openjdk
 brew_cask_install_if_missing adoptopenjdk8
 brew_cask_install_if_missing android-sdk
