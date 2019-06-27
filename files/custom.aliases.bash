a1ea7ed9b859135c1b39cd86387c15b307682dcc
Austin Kihara and James Hart <austin.kihara+james@voom.flights>
2019-06-27 10:07:01 -0700

    Heroku login (with expiration) and logout

--- files/custom.aliases.bash
+++ files/custom.aliases.bash
@@ -4,5 +4,6 @@ alias daily='bundle && yarn && rails voom:open voom:refresh && lint && specs && 
 alias lint='rubocop && yarn eslint .'
 alias cleandb='rails db:reset voom:open voom:refresh voom:create_test_promotion'
 alias specs='rails db:migrate && rails db:seed && rails db:test:prepare && rails parallel:prepare && rails parallel:spec'
-
+alias hlogin='heroku auth:login --expires-in=28800'
+alias hlogout='heroku auth:logout'
 
