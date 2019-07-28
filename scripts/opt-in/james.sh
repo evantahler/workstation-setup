82f1ffb025e1299e543a3f2452b74661e982ead3
James Hart <james@voom.flights>
2019-07-28 14:35:54 -0700

    Correct personal workstation setup

--- scripts/opt-in/james.sh
+++ scripts/opt-in/james.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/env bash
-# Usage: ./scripts/opt-in/james.sh
-# Don't stop if James' personal setup fails
-set +e
-
-source scripts/helpers/brew.sh
-
 bundle config build.libxml-ruby --use-system-libraries=true --with-xml2-include=/usr/local/opt/libxml2/include/libxml2/
+source scripts/helpers/brew.sh
 
 brew_install_if_missing mas
 brew_cask_install_if_missing spotify
