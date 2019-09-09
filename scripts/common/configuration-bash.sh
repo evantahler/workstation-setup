7447b8ce92e780c73a9897139417f168f02088b3
Evan Tahler <evan@evantahler.com>
2019-09-08 21:27:57 -0700

    source newly installed bash-it fully

--- scripts/common/configuration-bash.sh
+++ scripts/common/configuration-bash.sh
@@ -6,10 +6,13 @@ brew_install_if_missing grc
 brew_install_if_missing coreutils
 brew_install_if_missing rbenv
 brew_install_if_missing watch
+
 rm -rf ~/.bash_it
+export BASH_IT="$HOME/.bash_it"
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
 source ~/.bash_profile
+source ~/.bash_it/bash_it.sh
 
 bash-it enable completion git
 bash-it enable plugin ssh
